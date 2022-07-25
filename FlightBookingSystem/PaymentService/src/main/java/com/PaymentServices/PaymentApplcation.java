961a72a6ba9ed318525231c5c15a6b0efcade545
Aakritigenius <71003063+Aakritigenius@users.noreply.github.com>
2022-07-25 17:52:45 +0530

    Add files via upload

---
+++ FlightBookingSystem/PaymentService/src/main/java/com/PaymentServices/PaymentApplcation.java
@@ -0,0 +1,11 @@
+package com.PaymentServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplcation {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplcation.class,args);
+    }
+}
---
+++ FlightBookingSystem/SearchFlight/src/main/java/com/searchFlight/SeachfligtApplication.java
@@ -0,0 +1,11 @@
+package com.searchFlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeachfligtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeachfligtApplication.class,args);
+    }
+}
